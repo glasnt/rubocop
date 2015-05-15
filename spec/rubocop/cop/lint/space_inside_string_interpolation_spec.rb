76bffa48249f946ec572611964c3d0f446d727ff
Katie McLaughlin <katie@glasnt.com>
2015-05-15 10:03:45 +1000

    Update rspec for SpaceInsideStringInterpolation
    More tests, and testing auto correction

--- spec/rubocop/cop/lint/space_inside_string_interpolation_spec.rb
+++ spec/rubocop/cop/lint/space_inside_string_interpolation_spec.rb
@@ -5,20 +5,42 @@ require 'spec_helper'
 describe RuboCop::Cop::Lint::SpaceInsideStringInterpolation do
   subject(:cop) { described_class.new }
 
-  it 'registers an offense for spaces inside string interpolation' do
-    inspect_source(cop, '"This is a #{ string} inside."')
-    expect(cop.messages).to eq(['Space inside string interpolation detected.'])
+  context 'for ill-formatted string interpolations' do
+    let(:source) do
+      ['"#{ var}"',
+       '"#{ var }"',
+       '"#{var }"',
+       '"#{   var   }"']
+    end
+
+    let(:corrected_source) { '"#{var}"' }
+
+    it 'registers an offense for any variation of spaces inside the braces' do
+      inspect_source(cop, source)
+      expect(cop.messages)
+        .to eq(['Space inside string interpolation detected.'] * 4)
+    end
+
+    it 'auto-corrects spacing within a string interpolation' do
+      new_source = autocorrect_source(cop, source)
+      expect(new_source).to eq(([corrected_source] * 4).join("\n"))
+    end
   end
 
-  it 'corrects spaces inside string interpolation' do
-    corrected = autocorrect_source(cop, ['"some   #{ something} something"'])
-    expect(corrected).to eq '"some   #{something} something"'
-  end
+  context 'for well-formatted string interpolations' do
+    let(:source) do
+      ['"Variable is    #{var}      "',
+       '"  Variable is  #{var}"']
+    end
 
-  it 'registers an offense for variations of spaces inside string interpolation' do
-    inspect_source(cop, ['"#{A }"',
-                         '"#{ A }"',
-                         '"#{ A}"'])
-    expect(cop.messages).to eq(['Space inside string interpolation detected.'] * 3)
+    it 'doesnt register an offense for excess literal spacing' do
+      inspect_source(cop, source)
+      expect(cop.messages).to be_empty
+    end
+
+    it 'doesnt correct valid string interpolations' do
+      new_source = autocorrect_source(cop, source)
+      expect(new_source).to eq(source.join("\n"))
+    end
   end
 end
